21b8e508206c8434f087005e35a45da9a01f4551
AngryPanda <924193579guge@gmail.com>
2020-03-08 18:27:15 +0800

    添加index.js，修改package.json

---
+++ index.js
@@ -0,0 +1,15 @@
+import Button from './src/button'
+import ButtGroup from './src/button-group'
+import Icon from './src/icon'
+import Input from './src/input'
+import Row from './src/row'
+import Col from './src/col'
+import Header from './src/header'
+import Content from './src/content'
+import Sider from './src/sider'
+import Footer from "./src/footer"
+import Layout from "./src/layout"
+import Collapse from "./src/collapse"
+import CollapseItem from './src/collapse-item'
+
+export {Button, ButtGroup, Icon, Input, Row, Col, Header, Content, Sider, Footer, Layout, Collapse, CollapseItem}
